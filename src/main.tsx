0ff0a83a33245ce47e3c08bb0bbbe7f0116b8bfa
Franky <f.riosru@outlook.com>
2025-10-11 06:10:11 +0200

    Resolver conflicto en main.tsx usando versión App

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,12 @@
 ﻿import React from "react"
 import { createRoot } from "react-dom/client"
 import "./styles.css"
-<<<<<<< Updated upstream
 import App from "./App"
 import './sync/autosync'
-=======
-import Layout from "./src/modules/app/Layout"
->>>>>>> Stashed changes
 
 createRoot(document.getElementById("root")!)
   .render(
     <React.StrictMode>
-<<<<<<< Updated upstream
       <App />
-=======
-      <Layout />
->>>>>>> Stashed changes
     </React.StrictMode>
   )
